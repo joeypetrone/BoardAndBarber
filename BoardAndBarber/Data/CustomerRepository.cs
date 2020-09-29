103b39149b95e27e2761f7450eb7c14ec1c0569f
Joey Petrone <joey.petrone@gmail.com>
2020-09-28 21:08:45 -0500

    Database connected via Microsoft.Data.SQLClient

--- BoardAndBarber/Data/CustomerRepository.cs
+++ BoardAndBarber/Data/CustomerRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace BoardAndBarber.Data
 {
@@ -31,7 +32,43 @@ namespace BoardAndBarber.Data
 
         public Customer GetById(int id)
         {
-            return _customers.FirstOrDefault(c => c.Id == id);
+            var connection = new SqlConnection("Server = localhost; Database = BoardAndBarber; Trusted_Connection = True;");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            var query = $@"select *
+                          from Customers
+                          where id = {id}";
+
+            command.CommandText = query;
+
+            //run this query, and i don't care about the results
+            //command.ExecuteNonQuery();
+
+            //run this query, and only return the top row's leftmost column
+            //command.ExecuteScalar();
+
+            //run this query and give me the results one row at a time
+            var reader = command.ExecuteReader();
+            //sql server has executed the command and is waiting to give us results
+            //reader.Read(); <-- boolean value based on if data was found in database
+
+            if (reader.Read())
+            {
+                var customerFromDb = new Customer();
+                //do something
+                customerFromDb.Id = (int) reader["Id"]; //explicit conversion/cast, throws exception on failure
+                customerFromDb.Name = reader["Name"] as string; // implicit cast/conversion, returns a null on failure
+                customerFromDb.Birthday = DateTime.Parse(reader["Birthday"].ToString()); //parsing
+                customerFromDb.FavoriteBarber = reader["FavoriteBarber"].ToString(); //make it a string
+                customerFromDb.Notes = reader["Notes"].ToString();
+
+                return customerFromDb;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public Customer Update(int id, Customer customer)
