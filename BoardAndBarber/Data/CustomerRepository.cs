ba3669f4e45fc5c3f3dac0e14dfdf26eaa9c69cc
Joey Petrone <joey.petrone@gmail.com>
2020-10-03 10:58:04 -0500

    Add, Update, and Delete using Dapper

--- BoardAndBarber/Data/CustomerRepository.cs
+++ BoardAndBarber/Data/CustomerRepository.cs
@@ -25,19 +25,9 @@ namespace BoardAndBarber.Data
                          VALUES
                                (@name, @birthday, @favoritebarber, @notes)";
 
-            using var connection = new SqlConnection(_connectionString);
-            connection.Open();
+            using var db = new SqlConnection(_connectionString);
 
-            var cmd = connection.CreateCommand();
-            cmd.CommandText = sql;
-
-            cmd.Parameters.AddWithValue("name", customerToAdd.Name);
-            cmd.Parameters.AddWithValue("birthday", customerToAdd.Birthday);
-            cmd.Parameters.AddWithValue("favoritebarber", customerToAdd.FavoriteBarber);
-            cmd.Parameters.AddWithValue("notes", customerToAdd.Notes);
-
-            //run this query, and only return the top row's leftmost column
-            var newId = (int) cmd.ExecuteScalar();
+            var newId = db.ExecuteScalar<int>(sql, customerToAdd);
 
             customerToAdd.Id = newId;
         }
@@ -76,61 +66,33 @@ namespace BoardAndBarber.Data
                         Output inserted.*
                         WHERE id = @id";
 
-            using var connection = new SqlConnection(_connectionString);
-            connection.Open();
+            using var db = new SqlConnection(_connectionString);
+            
+            // if property names should match the name of the property or variable on the right, you don't have to specify.
+            // you can leave off the property name if the name on the right and the propery name match.
+            var parameters = new 
+            { 
+                customer.Name, // Name = customer.Name
+                customer.Birthday, // Birthday = customer.Birthday
+                customer.FavoriteBarber, // FavoriteBarber = customer.FavoriteBarber
+                customer.Notes, // Notes = customer.Notes
+                id // id = id
+            }; 
 
-            var cmd = connection.CreateCommand();
-            cmd.CommandText = sql;
+            var updatedCustomer = db.QueryFirstOrDefault<Customer>(sql, parameters);
 
-            cmd.Parameters.AddWithValue("name", customer.Name);
-            cmd.Parameters.AddWithValue("birthday", customer.Birthday);
-            cmd.Parameters.AddWithValue("favoritebarber", customer.FavoriteBarber);
-            cmd.Parameters.AddWithValue("notes", customer.Notes);
-            cmd.Parameters.AddWithValue("id", id);
-
-            var reader = cmd.ExecuteReader();
-
-            if(reader.Read())
-            {
-                return MapToCustomer(reader);
-            }
-
-            return null;
+            return updatedCustomer;
         }
 
-        public void Remove(int id)
+        public void Remove(int customerId)
         {
             var sql = @"DELETE 
                         FROM [dbo].[Customers]
                         WHERE Id = @id";
 
-            using var connection = new SqlConnection(_connectionString);
-            connection.Open();
+            using var db = new SqlConnection(_connectionString);
 
-            var cmd = connection.CreateCommand();
-            cmd.CommandText = sql;
-
-            cmd.Parameters.AddWithValue("id", id);
-
-            var rows = cmd.ExecuteNonQuery();
-
-            if(rows != 1)
-            {
-                //do something becuase that's bad
-            }
-        }
-
-        Customer MapToCustomer(SqlDataReader reader)
-        {
-            var customerFromDb = new Customer();
-            //do something with one result
-            customerFromDb.Id = (int)reader["Id"]; //explicit conversion/cast, throws exception on failure
-            customerFromDb.Name = reader["Name"] as string; // implicit cast/conversion, returns a null on failure
-            customerFromDb.Birthday = DateTime.Parse(reader["Birthday"].ToString()); //parsing
-            customerFromDb.FavoriteBarber = reader["FavoriteBarber"].ToString(); //make it a string
-            customerFromDb.Notes = reader["Notes"].ToString();
-
-            return customerFromDb;
+            db.Execute(sql, new { id = customerId});
         }
     }
 }
