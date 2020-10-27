189f19839c53d3edf36c5d056a11291cc85ab96f
Joey Petrone <joey.petrone@gmail.com>
2020-10-27 18:59:57 -0500

    one more file, oops

--- boardandbarber.ui/src/helpers/data/customerData.js
+++ boardandbarber.ui/src/helpers/data/customerData.js
@@ -4,10 +4,7 @@ import {baseUrl} from '../data/constants.json';
 const getAllCustomers = () => new Promise((resolve,reject) => {
 
   axios.get(`${baseUrl}/Customers`)
-    .then(response => {
-      const customers = response.data;
-      resolve(customers);
-    })
+    .then(response => resolve(response.data))
     .catch(reject);
 });
 
